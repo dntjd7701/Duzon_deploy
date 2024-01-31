5f67f84b7d11b30c06da902ee9dc233d39fa85ff
강우성 <peacegrace7701@douzone.com>
2024-01-31 14:13:03 +0900

    ripple animation done

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,22 +1,25 @@
 import { BellIcon, Cog6ToothIcon } from '@heroicons/react/24/solid';
-import { useEffect } from 'react';
 
 export const Navbar = () => {
-  useEffect(() => {
-    document.addEventListener('animationend', () => {
-
-    })
-    return () => {
-      // document.removeEventListener('animationend', )
-    }
-  }, [])
-
-
   //나중에 ripple 넣어보자
   const handleFileSettingBtnClick: React.MouseEventHandler = (e) => {
-    console.debug('e:', e)
-    
+    const btnElement = e.target as HTMLElement;
+    const [ x, y ] = [ e.clientX - btnElement.offsetLeft, e.clientY - btnElement.offsetTop ]
+
+    const animationElement = document.createElement('span');
+    animationElement.style.left = x + 'px'; 
+    animationElement.style.top = y + 'px'; 
+    animationElement.className = 'ripple-btn';
+
+    btnElement.appendChild(animationElement);
+    removeElement(animationElement);
   };
+
+  const removeElement = (animationElement: HTMLElement) => {
+    setTimeout(() => {
+      animationElement.parentNode?.removeChild(animationElement);
+    }, 500)
+  }
 
   return (
     <div className="px-10 sticky top-4 z-50">
@@ -25,12 +28,11 @@ export const Navbar = () => {
           <div className="h-full w-full px-5 py-2 flex flex-wrap items-center justify-between text-white">
             <span className="font-bold text-inherit mr-4 ml-2">설정</span>
             <div className="flex ml-auto gap-1.5 md:mr-4">
-              <button
-                id='ripple-btn'
-                className="relative text-white font-bold h-full bg-black py-3 px-6 text-xs hover:opacity-80 active:bg-white/30 active:shadow-button_active rounded-lg shadow-button active:animation-ripple"
-                onClick={handleFileSettingBtnClick}>
-                배포 파일 세팅하기
-              </button>
+                <button
+                  className="relative overflow-hidden text-white font-bold h-full bg-black py-3 px-6 text-xs hover:opacity-80 active:bg-white/30 active:shadow-button_active rounded-lg shadow-button active:animation-ripple"
+                  onClick={handleFileSettingBtnClick}>
+                  배포 파일 세팅하기
+                </button>
               <button className="w-10 h-10 hover:bg-white/10 active:bg-white/30 rounded-lg">
                 <Cog6ToothIcon className="h-4 w-full" />
               </button>
